7c8e6ae524d499e84730af357f87e5eea5cbcadd
Jericho Pasco <112312834+Jpasco31@users.noreply.github.com>
2025-02-21 18:40:20 +0800

    Refactor DropdownLink to support dynamic components
    Replaced <Link> with a dynamic component to allow flexibility between links and buttons. Added optional "method" and "as" props to extend functionality and improve customization for Inertia.js usage. This enhances adaptability without breaking existing behavior.

--- resources/js/Components/DropdownLink.vue
+++ resources/js/Components/DropdownLink.vue
@@ -1,16 +1,21 @@
-<script setup lang="ts">
+<script lang="ts" setup>
 import { Link } from '@inertiajs/vue3';
 
 defineProps<{
-    href: string;
+    href?: string; // href might be undefined
+    method?: string; // allow a method for Inertia
+    as?: string; // let the user specify "button" if they want
 }>();
 </script>
 
 <template>
-    <Link
+    <component
+        :is="href ? Link : 'button'"
+        :as="as"
         :href="href"
+        :method="method"
         class="block w-full px-4 py-2 text-start text-sm leading-5 text-gray-700 transition duration-150 ease-in-out hover:bg-gray-100 focus:bg-gray-100 focus:outline-none dark:text-gray-300 dark:hover:bg-gray-800 dark:focus:bg-gray-800"
     >
         <slot />
-    </Link>
+    </component>
 </template>
